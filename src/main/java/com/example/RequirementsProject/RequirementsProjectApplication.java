f3af0966d1f2a3bcf77394c12135c99bc48eb81e
raduceaca1234 <ceaca.radu@yahoo.com>
2022-01-05 18:30:18 +0200

    Inital commit

---
+++ src/main/java/com/example/RequirementsProject/RequirementsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.RequirementsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequirementsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequirementsProjectApplication.class, args);
+	}
+
+}
